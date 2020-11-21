755a3f07e74999ac0443c2954cff9554d7b07a17
Daniel-203 <Daniel.Pruys@gmx.de>
2020-11-21 23:42:57 +0100

    change avgCons. = float, FÜ Java v0.3

--- Tankverhalten/app/src/main/java/com/example/tankverhalten/fahrzeuguebersicht_fragment.java
+++ Tankverhalten/app/src/main/java/com/example/tankverhalten/fahrzeuguebersicht_fragment.java
@@ -16,7 +16,10 @@ public class fahrzeuguebersicht_fragment extends Fragment {
     View view;
     public fahrzeuguebersicht_fragment() {}
     public TextView licensePlate;
-    public TextView avgConsumption;
+    public TextView consumption;
+    public TextView co2;
+    public TextView range;
+    public TextView fuel;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Nullable
@@ -24,13 +27,23 @@ public class fahrzeuguebersicht_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fahrzeuguebersicht_layout, container, false);
 
-        Vehicle r = new Vehicle("Test1", "123", 0, 0, 0, 0, 0, 0, VehicleType.CAR);
+        Vehicle r = new Vehicle("Test1", "123", 0, 500, 250, 35000, 75, (float) 7.5, VehicleType.CAR);
 
         licensePlate = (TextView) view.findViewById(R.id.show_licensePlate);
         licensePlate.setText(r.licensePlate);
 
-        avgConsumption = (TextView) view.findViewById(R.id.show_averageConsumption);
-        avgConsumption.setText(String.valueOf(r.averageConsumption));
+        consumption = (TextView) view.findViewById(R.id.show_averageConsumption);
+        consumption.setText(String.valueOf(r.averageConsumption));
+
+        co2 = (TextView) view.findViewById(R.id.show_co2emissions);
+        co2.setText(String.valueOf(r.co2emissions));
+
+        range = (TextView) view.findViewById(R.id.show_remainingRange);
+        range.setText(String.valueOf(r.remainingRange));
+
+        fuel = (TextView) view.findViewById(R.id.show_fuelLevel);
+        fuel.setText(String.valueOf(r.fuelLevel));
+
         return view;
     }
     /*
