b16d057b093875fef5f5127b6030649e9d8aa775
Daniel-203 <Daniel.Pruys@gmx.de>
2020-11-22 00:34:43 +0100

    FÜ Java v0.4

--- Tankverhalten/app/src/main/java/com/example/tankverhalten/fahrzeuguebersicht_fragment.java
+++ Tankverhalten/app/src/main/java/com/example/tankverhalten/fahrzeuguebersicht_fragment.java
@@ -1,5 +1,6 @@
 package com.example.tankverhalten;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,6 +12,9 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 public class fahrzeuguebersicht_fragment extends Fragment {
 
     View view;
@@ -20,6 +24,7 @@ public class fahrzeuguebersicht_fragment extends Fragment {
     public TextView co2;
     public TextView range;
     public TextView fuel;
+    public TextView mileAge;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Nullable
@@ -29,25 +34,29 @@ public class fahrzeuguebersicht_fragment extends Fragment {
 
         Vehicle r = new Vehicle("Test1", "123", 0, 500, 250, 35000, 75, (float) 7.5, VehicleType.CAR);
 
+        DecimalFormat df = new DecimalFormat("#.##");
+        DecimalFormatSymbols dfs = df.getDecimalFormatSymbols();
+        dfs.setDecimalSeparator(',');
+        dfs.setGroupingSeparator('.');
+
         licensePlate = (TextView) view.findViewById(R.id.show_licensePlate);
-        licensePlate.setText(r.licensePlate);
+        licensePlate.setText(df.format(r.licensePlate));
 
         consumption = (TextView) view.findViewById(R.id.show_averageConsumption);
-        consumption.setText(String.valueOf(r.averageConsumption));
+        consumption.setText(df.format(r.averageConsumption));
 
         co2 = (TextView) view.findViewById(R.id.show_co2emissions);
-        co2.setText(String.valueOf(r.co2emissions));
+        co2.setText(df.format(r.co2emissions));
 
         range = (TextView) view.findViewById(R.id.show_remainingRange);
-        range.setText(String.valueOf(r.remainingRange));
+        range.setText(df.format(r.remainingRange));
 
         fuel = (TextView) view.findViewById(R.id.show_fuelLevel);
-        fuel.setText(String.valueOf(r.fuelLevel));
+        fuel.setText(df.format(r.fuelLevel));
+
+        mileAge = (TextView) view.findViewById(R.id.show_mileAge);
+        mileAge.setText(df.format(r.mileAge));
 
         return view;
     }
-    /*
-        Hier kommt die Fahrzeugübersicht Activity hin.
-    */
-
 }
