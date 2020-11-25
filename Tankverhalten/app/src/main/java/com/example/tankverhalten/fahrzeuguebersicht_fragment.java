158544f6efea6dbdea8947294fa94fcc0796b595
Daniel-203 <Daniel.Pruys@gmx.de>
2020-11-25 15:58:11 +0100

    FÜ: added TÜV, HU

--- Tankverhalten/app/src/main/java/com/example/tankverhalten/fahrzeuguebersicht_fragment.java
+++ Tankverhalten/app/src/main/java/com/example/tankverhalten/fahrzeuguebersicht_fragment.java
@@ -14,6 +14,8 @@ import androidx.fragment.app.Fragment;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class fahrzeuguebersicht_fragment extends Fragment {
 
@@ -25,6 +27,8 @@ public class fahrzeuguebersicht_fragment extends Fragment {
     public TextView range;
     public TextView fuel;
     public TextView mileAge;
+    public TextView inspection;
+    public TextView permission;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Nullable
@@ -33,6 +37,8 @@ public class fahrzeuguebersicht_fragment extends Fragment {
         view = inflater.inflate(R.layout.fahrzeuguebersicht_layout, container, false);
 
         Vehicle r = new Vehicle("Test1", "123", 0, (float) 1500.69, 250, 35000, (float) 75.3, (float) 7.5, VehicleType.CAR);
+        r.inspection = LocalDate.of(2020, 12, 21);
+        r.permission = LocalDate.of(2021, 2, 10);
 
         // Format of displayed Number
         DecimalFormat df = new DecimalFormat("#,###.##");
@@ -44,6 +50,10 @@ public class fahrzeuguebersicht_fragment extends Fragment {
         // int to float for NumberFormat
         float fRange = (float) r.remainingRange;
         float miles = (float) r.mileAge;
+
+        // Date to String
+        String strInspection = r.inspection.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String strPermission = r.permission.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         // lock TextView object with TextView(id) from xml, then setText
         licensePlate = (TextView) view.findViewById(R.id.show_licensePlate);
@@ -63,6 +73,12 @@ public class fahrzeuguebersicht_fragment extends Fragment {
 
         mileAge = (TextView) view.findViewById(R.id.show_mileAge);
         mileAge.setText(df.format(miles));
+
+        inspection = (TextView) view.findViewById(R.id.show_nextInspection);
+        inspection.setText(strInspection);
+
+        permission = (TextView) view.findViewById(R.id.show_nextPermission);
+        permission.setText(strPermission);
 
         return view;
     }
